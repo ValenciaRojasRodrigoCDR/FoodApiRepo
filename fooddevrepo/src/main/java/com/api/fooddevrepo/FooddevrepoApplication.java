632ad61e91b8437ab95559932df76a46d0833765
ValenciaRojasRodrigoCDR <59927569+ValenciaRojasRodrigoCDR@users.noreply.github.com>
2025-04-06 18:27:12 +0200

    (REVR) Create first on Spring REPO

---
+++ fooddevrepo/src/main/java/com/api/fooddevrepo/FooddevrepoApplication.java
@@ -0,0 +1,13 @@
+package com.api.fooddevrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddevrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddevrepoApplication.class, args);
+	}
+
+}
---
+++ fooddevrepo/src/test/java/com/api/fooddevrepo/FooddevrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fooddevrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddevrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
